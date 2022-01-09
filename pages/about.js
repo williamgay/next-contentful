3f53b9973695bd9c36452bc1436d222aaa69a766
William Gay <william.gay@paceco.com>
2022-01-08 19:09:46 -0500

    New Env Example

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Meta from "../components/Meta";
+
+const about = () => {
+  return (
+    <div className="container">
+      <Meta title="About" />
+      <h1>About Us Page</h1>
+    </div>
+  );
+};
+
+export default about;
